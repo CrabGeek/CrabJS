8d4695d346aa0e8c81dff021005da661d26246ac
GuipengTT <xgp980303@gmail.com>
2021-07-27 00:08:44 +0800

    add put

--- core.js
+++ core.js
@@ -54,13 +54,13 @@
          this.keyRange = env.IDBKeyRange;
 
          this.consts = {
-             'READ_ONLY': 'readonly',
-             'READ_WRITE': 'readwrite',
-             'VERSION_CHANGE': 'versionchange',
-             'NEXT': 'next',
-             'NEXT_NO_DUPLICATE': 'nextunique',
-             'PREV': 'prev',
-             'PREV_NO_DUPLICATE': 'prevunique'
+             READ_ONLY: 'readonly',
+             READ_WRITE: 'readwrite',
+             VERSION_CHANGE: 'versionchange',
+             NEXT: 'next',
+             NEXT_NO_DUPLICATE: 'nextunique',
+             PREV: 'prev',
+             PREV_NO_DUPLICATE: 'prevunique'
          };
 
          this.openDB();
@@ -90,20 +90,97 @@
                      if (preventSuccessCallBack) {
                          return;
                      }
-
                      if (this.db) {
                          this.onStoreReady();
                      }
                      this.db = event.target.result;
 
-
                  }.bind(this);
 
                  openRequest.onerror = function(errorEvent) {
-                     this.onError(new Error(`Crabjs Error: ${errorEvent.target.error}`))
+                     this.onError(new Error(`Crabjs Error: ${errorEvent.target.error}`));
                  }.bind(this);
 
-                 openRequest.onupgradeneeded = function() {}
+                 openRequest.onupgradeneeded = function(event) {
+                     this.db = event.target.result;
+                     if (this.db.objectStoreNames.contain(this.storeName)) {
+                         this.store = event.target.transcation.objectStore(this.storeName);
+                     } else {
+                         var optionalParameters = { autoIncrement: true };
+                         if (this.keyPath != null) {
+                             optionalParameters.keyPath = this.keyPath;
+                         }
+                         this.store = this.db.createObjectStore(this.storeName, optionalParameters);
+                     }
+                 }.bind(this);
+             },
+
+             deleteDataBase: function(onSuccess, onError) {
+                 if (this.idb.deleteDataBase) {
+                     this.db.close();
+                     var deleteRequest = this.idb.deleteDataBase(this.dbName);
+                     deleteRequest.onSuccess = onSuccess;
+                     deleteRequest.onError = onError;
+                 } else {
+                     onError(new Error("Current Browser does not support delete database"));
+                 }
+             },
+
+             /**
+              * 操作数据
+              */
+
+             /**
+              * @description: 添加或更新数据
+              * @param {*} key
+              * @param {*} value
+              * @param {*} onSuccess
+              * @param {*} onError
+              * @return {*}
+              */
+             put: function(key, value, onSuccess, onError) {
+                 if (this.keyPath != null) {
+                     onSuccess = onSuccess;
+                     onError = onError;
+                     value = key;
+                 }
+
+                 onSuccess || (onSuccess = defaultSuccessHandler);
+                 onError || (onError = defaultErrorHandler);
+
+                 var hasSucess = false,
+                     result = null,
+                     putRequest;
+
+                 var putTransaction = this.db.transcation([this.storeName], this.consts.READ_WRITE);
+
+             },
+
+             /**
+              * @description: 获取数据 
+              * @param {*}
+              * @return {*}
+              */
+             get: function() {
+
+             },
+
+             /**
+              * @description: 删除数据 
+              * @param {*}
+              * @return {*}
+              */
+             remove: function() {},
+
+             /**
+              * 索引
+              */
+             getIndexList: function() {
+                 return this.store.indexNames;
+             },
+
+             hasIndex: function(indexName) {
+                 return this.store.indexNames.contain(indexName);
              }
          }
 
